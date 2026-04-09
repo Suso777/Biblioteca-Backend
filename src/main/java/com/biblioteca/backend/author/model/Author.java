f09f2bb65530f86550dc0030adfd1293be37064a
David-Navarro-Oliver <navarropalle@gmail.com>
2026-04-09 11:52:39 +0200

    feat(author): add one-to-many relationship with Book

--- src/main/java/com/biblioteca/backend/author/model/Author.java
+++ src/main/java/com/biblioteca/backend/author/model/Author.java
@@ -1,12 +1,17 @@
 package com.biblioteca.backend.author.model;
 
+import com.biblioteca.backend.book.model.Book;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -38,4 +43,8 @@ public class Author {
 
     @NotNull
     private Boolean alive;
+
+    @OneToMany(mappedBy = "author")
+    @JsonIgnoreProperties("author")
+    private List<Book> books = new ArrayList<>();
 }
