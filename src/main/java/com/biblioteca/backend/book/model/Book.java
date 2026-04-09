5d49f07754576621c4de871c53e74c54f71adfb8
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2026-04-09 11:58:01 +0200

    fix change book model to match in author model

--- src/main/java/com/biblioteca/backend/book/model/Book.java
+++ src/main/java/com/biblioteca/backend/book/model/Book.java
@@ -4,77 +4,37 @@ package com.biblioteca.backend.book.model;
 import com.biblioteca.backend.author.model.Author;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String title;
+    @NotBlank
     private String ISBN;
+    @NotNull
     private int publicationYear;
+    @NotBlank
     private String image;
 
+    @Setter
     @ManyToOne
     @JoinColumn(name = "author_id")
     @JsonIgnoreProperties("products")
     private Author author;
-
-    public Book(String title, String ISBN, int publicationYear, String image, Author author) {
-        this.title = title;
-        this.ISBN = ISBN;
-        this.publicationYear = publicationYear;
-        this.image = image;
-        this.author = author;
-    }
-
-    public Book() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getISBN() {
-        return ISBN;
-    }
-
-    public void setISBN(String ISBN) {
-        this.ISBN = ISBN;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public int getPublicationYear() {
-        return publicationYear;
-    }
-
-    public void setPublicationYear(int publicationYear) {
-        this.publicationYear = publicationYear;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
-    }
 }
